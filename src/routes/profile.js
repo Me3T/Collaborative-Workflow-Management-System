e25f2daa7d10298c2b11b96bcb400ac993260c88
Me3T <rai.manmeet1998@gmail.com>
2025-06-22 18:35:04 +0530

    bug fix

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -8,7 +8,7 @@ profileRouter.get("/profile/view", userAuth, async (req, res) => {
     const user = req.user;
     res.send(user);
   } catch (err) {
-    res.status(400).send("Error: " + err.message);
+    res.status(400).send("Error Occurred " + err.message);
   }
 });
 
