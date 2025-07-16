8bc0c660005b439b4fac8bded19c2e299b325019
Me3T <rai.manmeet1998@gmail.com>
2025-07-16 19:08:00 +0530

    .

--- src/models/project.js
+++ src/models/project.js
@@ -21,7 +21,7 @@ const projectSchema = new mongoose.Schema({
   },
   status: {
     type: String,
-    enum: ["Open", "in-progress", "completed"],
+    enum: ["Open", "In-Progress", "completed"],
     default: "Open",
   },
   teamMembers: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
