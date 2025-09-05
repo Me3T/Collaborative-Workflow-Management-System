a7d6c6947efc5e64bcf8a1277c064ad009f5e054
Me3T <rai.manmeet1998@gmail.com>
2025-09-05 19:27:05 +0530

    ..

--- src/models/task.js
+++ src/models/task.js
@@ -10,7 +10,7 @@ const taskSchema = new mongoose.Schema({
   description: {
     type: String,
     required: false,
-    maxLength: 1100,
+    maxLength: 1200,
   },
   assignedUser: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
   priority: {
